c7efc3e89b7a1eb60e7c7230c85e07b418d8d4d6
Mahdi Shams <mahdi.shams@daimler.com>
2018-09-24 21:52:03 +0200

    added support for respecting db tag and also detect type change

--- schema.go
+++ schema.go
@@ -11,6 +11,8 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+//TODO support array filedName
+//TODO detect field type change
 func (dam *BasicDAM) SyncDB() error {
 	var err error
 	err = addTable(dam.TableName(), dam.DB, dam.Instance)
@@ -49,11 +51,11 @@ func addTable(tablename string, db *sqlx.DB, instance interface{}) error {
 				"type of field: %s can not be inferred. if it is a struct please add dbtype tag", typeField.Name))
 		}
 		//log.Infof("field info, name:%s, type: %s, pgtype:%s", typeField.Name, typeOfField, typ)
-
+		fieldName := getPgName(typeField.Name, dbtag)
 		if typeField.Name == "ID" {
-			strQ = strQ + " " + typeField.Name + " SERIAL PRIMARY KEY " + ","
+			strQ = strQ + " " + fieldName + " SERIAL PRIMARY KEY " + ","
 		} else {
-			strQ = strQ + " " + typeField.Name + " " + typ + ","
+			strQ = strQ + " " + fieldName + " " + typ + ","
 		}
 
 	}
@@ -63,6 +65,18 @@ func addTable(tablename string, db *sqlx.DB, instance interface{}) error {
 	_, err := db.Exec(strQ)
 	return err
 }
+func getPgName(name, dbtag string) string {
+	if dbtag != "" {
+		return dbtag
+	}
+	//postgres converts all names to lower case
+	return strings.ToLower(name)
+}
+
+type dbColumn struct {
+	Column_Name string
+	Data_Type   string
+}
 
 func syncSchema(tableName string, db *sqlx.DB, instance interface{}) error {
 	ival := reflect.ValueOf(instance)
@@ -71,48 +85,79 @@ func syncSchema(tableName string, db *sqlx.DB, instance interface{}) error {
 	}
 
 	//this map checks if the field exists on pg table or not
-	structFields := make(map[string]bool)
+	structFields := make(map[string]string)
 	for j := 0; j < ival.NumField(); j++ {
 		typeField := ival.Type().Field(j)
-		//postgres converts all names to lower case
-		filedName := strings.ToLower(typeField.Name)
-		//check if the field exists in db or not
-		exists, _ := checkPgColumn(tableName, db, filedName)
 		dbtag := typeField.Tag.Get("db")
-
-		//this is a hack to not to try to dropp the column later while it is not in db
-		if dbtag != "-" {
-			structFields[filedName] = true
+		if dbtag == "-" {
+			continue
 		}
-		//if it is not in db and not excluded from struct add field to database
-		if !exists && dbtag != "-" {
+		filedName := getPgName(typeField.Name, dbtag)
+		//check if the field exists in db or not
+		exists, err := checkPgColumn(tableName, db, filedName)
+		if err != nil {
+			return errors.New("can not check for existance of column, " + err.Error())
+		}
 
-			typ := goType2Pg(getFieldType(instance, typeField.Name))
-			log.Infof("adding field: %s", filedName)
+		typ := goType2Pg(getFieldType(instance, typeField.Name))
+		//we keep track of which filed must be present in db and of which data type
+		structFields[filedName] = typ
+		//if it is not in db we should create the column
+		if !exists {
+			log.Infof("adding field: %s to table %s", filedName, tableName)
 			err := addPostgresColumn(tableName, db, filedName, typ)
 			if err != nil {
-				return errors.New("can not add row to table: " + err.Error())
+				return errors.New("can not add column to table: " + err.Error())
 			}
 		}
 	}
-
-	var dbcols []string
+	// now try to remove extra fields
+	dbcolumns := make([]*dbColumn, 0)
 	//get list of table columns
-	err := db.Select(&dbcols, "SELECT Column_Name FROM information_schema.columns WHERE table_name='"+tableName+"'")
+	err := db.Select(&dbcolumns, "SELECT column_name, data_type FROM information_schema.columns WHERE table_name='"+tableName+"'")
 	if err != nil {
 		return err
 	}
-	for k := 0; k < len(dbcols); k++ {
+	for k := 0; k < len(dbcolumns); k++ {
 		//drop database column if don't exist in struct
-		_, exists := structFields[dbcols[k]]
-		if !exists {
-			log.Infof("dropping field: %s", dbcols[k])
-			dropPostgresColumn(tableName, db, dbcols[k])
+		desiredtype, shouldExist := structFields[dbcolumns[k].Column_Name]
+		if !shouldExist {
+			log.Infof("dropping field: %s from table %s", dbcolumns[k].Column_Name, tableName)
+			err := dropPostgresColumn(tableName, db, dbcolumns[k].Column_Name)
+			if err != nil {
+				return errors.New("can not remove column from table: " + err.Error())
+			}
+		}
+		//if type changes, we need to handle it
+		if shouldExist && desiredtype != dbcolumns[k].Data_Type {
+			log.Infof("type changed in field: %s in table %s from %s to %s",
+				dbcolumns[k].Column_Name, tableName, dbcolumns[k].Data_Type, desiredtype)
+			err := dropAddPostgresColumn(tableName, db, dbcolumns[k].Column_Name, desiredtype)
+			if err != nil {
+				return errors.New("can not remove/add column to table: " + err.Error())
+			}
 		}
 	}
 	return nil
 }
 
+func dropAddPostgresColumn(tableName string, db *sqlx.DB, field, dbtype string) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("alter table " + tableName + " drop column " + field)
+	if err != nil {
+		return err
+	}
+	defaultVal := getDefaultPgValue(dbtype)
+	strQ := "alter table " + tableName + " add column " + field + " " + dbtype + " default " + defaultVal
+	_, err = tx.Exec(strQ)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
 func checkPgColumn(tableName string, db *sqlx.DB, field string) (bool, error) {
 	var result int
 	err := db.Get(&result, "SELECT count(*) FROM information_schema.columns WHERE table_name='"+tableName+"' and column_name='"+field+"'")
@@ -130,7 +175,6 @@ func checkPgColumn(tableName string, db *sqlx.DB, field string) (bool, error) {
 func addPostgresColumn(tableName string, db *sqlx.DB, field, dbtype string) error {
 	//withut default value, older rows would be set to null which results in error when scanning
 	defaultVal := getDefaultPgValue(dbtype)
-
 	strQ := "alter table " + tableName + " add column " + field + " " + dbtype + " default " + defaultVal
 	_, err := db.Exec(strQ)
 	return err
@@ -149,8 +193,8 @@ func goType2Pg(typeOfField string) string {
 		pgtyp = "integer"
 	} else if typeOfField == "bool" {
 		pgtyp = "boolean"
-	} else if typeOfField == "JSONB" {
-		pgtyp = "JSONB"
+	} else if typeOfField == "jsonb" {
+		pgtyp = "jsonb"
 	} else if typeOfField == "time" {
 		pgtyp = "timestamp with time zone"
 	} else if typeOfField == "float32" || typeOfField == "float64" {
@@ -168,7 +212,7 @@ func getDefaultPgValue(pgtype string) string {
 		defaultVal = "0"
 	} else if pgtype == "boolean" {
 		defaultVal = "false"
-	} else if pgtype == "JSONB" {
+	} else if pgtype == "jsonb" {
 		defaultVal = "'{}'"
 	} else if pgtype == "timestamp with time zone" {
 		defaultVal = "'0001-01-01 03:25:44+03:25:44'"
