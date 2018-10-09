13bf7b630553133eac6f77122f4905acc08a8aee
Mahdi Shams <mahdi.shams@daimler.com>
2018-10-09 10:17:54 +0200

    added invalid error

--- errors.go
+++ errors.go
@@ -7,3 +7,4 @@ import (
 var ErrDB = errors.New("Database Operation Failed")
 var ErrNotFound = errors.New("Entity Not Found")
 var ErrFormat = errors.New("Data format is Wrong")
+var ErrInvalid = errors.New("Data is invalid")
